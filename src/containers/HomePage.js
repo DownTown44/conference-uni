ad9dd886ae06689966229089b2a9dd1640c7d00c
Apor Szabo <apor4444@gmail.com>
2021-05-13 10:24:33 +0300

    Some small esthetic changes

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-import Toolbar from '../components/Navigation/Toolbar';
-
 const HomePage = () => {
+
+
+
   return (
     <div className="homePage">
       <section className="homePage__welcomeSection">
         <div className="homePage__greeting">
           <h1>Welcome</h1>
-          <h2>Looking for some <span>inspiration</span>?</h2>
+          {/* some inspiration, a prize, development, a community, an experience, innovation */}
+          <h2>Looking for <span>inspiration</span>?</h2>
+          <h2>The possibilities are endless on the <span>BTW</span> conference.</h2>
         </div>
       </section>
     </div>
